4232f3f9b4ea2aa44f11bdaf2e28e204a36771ae
chenjq02 <chenjq02@qq.com>
2018-12-20 23:24:26 +0800

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		// $topics = Topic::paginate(30);
+        $topics = Topic::with('user', 'category')->paginate(30);//with预加载解决N+1问题
 		return view('topics.index', compact('topics'));
 	}
 
