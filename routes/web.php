c001578f619f60b6aaf14d21f218525f2f75765e
chenjq02 <chenjq02@qq.com>
2018-12-20 22:40:38 +0800

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
 // Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
